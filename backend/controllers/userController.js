66cd54c0cb573eea79c66e25a9785639053a213f
bala418 <balavtwo@gmail.com>
2023-05-08 12:01:03 +0530

    sign up and login backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+// login a user
+
+const loginUser = async (req, res) => {
+  const { email, password, blockChainAddress } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    console.log("User logged in", user, blockChainAddress);
+    res.status(200).json({ email, token, blockChainAddress });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// signup a user
+const signupUser = async (req, res) => {
+  const { email, password, blockChainAddress } = req.body;
+
+  try {
+    const user = await User.signup(email, password, blockChainAddress);
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token, blockChainAddress });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = { loginUser, signupUser };
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const workoutRouter = require("./routes/workouts");
+const userRoutes = require("./routes/user");
+// express app
+const app = express();
+const morgan = require("morgan");
+
+// middleware
+// used to parse the body of the request
+app.use(express.json());
+app.use(morgan("combined"));
+app.use(cors());
+// display a html file when the root path is hit
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+// used to parse the url
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// use the routes only when the following path is hit
+// app.use("/api/auction", workoutRouter);
+app.use("/api/user", userRoutes);
+
+// connect to the database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port 4000");
+    });
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
